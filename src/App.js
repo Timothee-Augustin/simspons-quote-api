0e553afc809fb75857b4a1c1139a6e00d6d4da75
Timothee <augustin.timothee@gmail.com>
2021-04-13 20:15:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
+import QuoteCard from "./components/QuoteCard"
 import './App.css';
 
+const sampleSimpson =  
+  { "quote": "I can't even say the word 'titmouse' without gigggling like a schoolgirl.",
+    "character": "Homer Simpson",
+    "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+    "characterDirection": "Right"}
+
+console.log(sampleSimpson)
+
 function App() {
+  
+  const [simpson, setSimpson] = useState(sampleSimpson)  
+
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+
+  const getQuote = () => {
+    fetch(url)
+    .then ((response) => response.json())    
+    .then((result) => {
+      console.log(result)    
+      setSimpson(result[0])
+    })
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteCard simpson = {simpson} />
+      <button type="button" onClick={getQuote}>New random quote</button>
     </div>
   );
 }
